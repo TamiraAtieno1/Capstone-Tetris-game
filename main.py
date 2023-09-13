808c4059bcf3f9a06fddd2ad3cc0fb99465a2a23
TamiraAtieno1 <tamiraatieno20@gmail.com>
2023-09-13 06:41:29 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import pygame,sys
+from game import Game
+
+pygame.font.init()
+pygame.init()
+dark_purple = (166, 0, 247)
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Python Tetris")
+
+clock = pygame.time.Clock()
+
+game = Game()
+
+GAME_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(GAME_UPDATE, 200)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if game.game_over == True:
+                game.game_over = False
+                game.reset()
+            if event.key == pygame.K_LEFT and game.game_over == False:
+                game.move_left()
+            if event.key == pygame.K_RIGHT and game.game_over == False:
+                game.move_right()
+            if event.key == pygame.K_DOWN and game.game_over == False:
+                game.move_down()
+                game.update_score(0, 1)
+            if event.key == pygame.K_UP and game.game_over == False:
+                game.rotate()
+        if event.type == GAME_UPDATE and game.game_over == False:
+            game.move_down()
+
+			
+    # Drawing
+    screen.fill(dark_purple)
+    game.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
