79280bb891ac167ffd71efdd10e2b1c5f1071482
TamiraAtieno1 <tamiraatieno20@gmail.com>
2023-09-15 16:40:26 +0300

    updates

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pygame,sys
 from game import Game
+from colors import Colors
 
 pygame.font.init()
 pygame.init()
-dark_purple = (166, 0, 247)
 
-screen = pygame.display.set_mode((300, 600))
+title_font = pygame.font.Font(None, 40)
+score_surface = title_font.render("Score", True, Colors.white)
+next_surface = title_font.render("Next", True, Colors.white)
+game_over_surface = title_font.render("GAME OVER", True, Colors.white)
+
+score_rect = pygame.Rect(320, 55, 170, 60)
+next_rect = pygame.Rect(320, 215, 170, 180)
+
+screen = pygame.display.set_mode((500, 620))
 pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
@@ -38,7 +46,19 @@ while True:
 
 			
     # Drawing
-    screen.fill(dark_purple)
+    score_value_surface = title_font.render(str(game.score), True, Colors.white)
+
+    screen.fill(Colors.dark_purple)
+    screen.blit(score_surface, (365, 20, 50, 50))
+    screen.blit(next_surface, (375, 180, 50, 50))
+
+    if game.game_over == True:
+        screen.blit(game_over_surface, (320, 450, 50, 50))
+
+    pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
+    screen.blit(score_value_surface, score_value_surface.get_rect(centerx = score_rect.centerx, 
+		centery = score_rect.centery))
+    pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
     game.draw(screen)
 
     pygame.display.update()
